22ff18db74721fce20ce47a254d7c7e3df6f752b
KhushiRaghuwanshi20 <khushiraghuwanshi020@gmail.com>
2026-01-13 23:17:02 +0530

    login-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+// Home page abhi dummy rakhte hain
+const Home = () => <div className="container"><h1>🏠 Home Feed</h1></div>
 
 function App() {
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>CampusFeedz Working! 🚀</h1>
-      <p>Tumhara Frontend Setup successful hai.</p>
-    </div>
+    <Router>
+      <Navbar />
+      <div style={{ marginTop: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/global.css' // Yahan humne apni CSS file jodi hai
+import './styles/global.css' 
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
